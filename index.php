fa0ebd6a345621cadbaeadd0744fa7a098f5f97c
mxrirxv <marav070614gmail.com>
2026-03-06 08:39:47 +0100

    v3

--- index.php
+++ index.php
@@ -53,8 +53,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo APP_NAME; ?> - Login</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="htdocs/css/style.css">
+    <link rel="stylesheet" href="htdocs/css/responsive.css">
     <style>
         .login-wrapper {
             min-height: 100vh;
